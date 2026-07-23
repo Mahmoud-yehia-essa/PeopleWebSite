c2c18afe643f96a56a61243bd0d1e4213327e28f
Mahmoud Yehia Essa <essa2002135@gmail.com>
2026-07-23 09:25:40 +0300

    feat: add facebook domain verification meta tag

--- resources/views/frontend/wiselook/master_dashboard.blade.php
+++ resources/views/frontend/wiselook/master_dashboard.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8"/>
     <meta content="width=device-width, initial-scale=1.0" name="viewport"/>
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="facebook-domain-verification" content="0v84hov7fx1htv4mzy3ta9qstb54z4" />
     @auth
         @vite(['resources/js/app.js'])
     @endauth
